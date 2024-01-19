c18695b04b95b9cf747519f08af808d8e678eb31
chenshengtian <501275851@qq.com>
2024-01-19 16:17:57 +0800

    init

---
+++ src/app/layout.jsx
@@ -0,0 +1,13 @@
+import { Provider } from './provider';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  )
+}
