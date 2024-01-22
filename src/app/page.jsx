4b93d129a44dc354911acbcb5d8fb0274ee89abe
chenshengtian <501275851@qq.com>
2024-01-22 13:50:32 +0800

    add router

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
+'use client'
 import {Button} from "@nextui-org/react";
 
+async function ngspice() {
+  const res = await fetch ('/api', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  })
+  console.log(await res.json());
+
+}
+
 export default function Page() {
-  return <Button>Press me</Button>
+  return (
+      <Button onPress={() => ngspice()}>Press me</Button>
+  )
 }
