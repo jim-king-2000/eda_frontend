d7085eab8f0c17f5ebafd558810370d56493e1f5
Jim Jin <jim_king_2000@163.com>
2024-01-22 15:25:36 +0800

    add xy chart

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,49 @@
 'use client';
 
 import { Button } from '@nextui-org/react';
+import { useState } from 'react';
+import { Axis, Grid, LineSeries, XYChart } from '@visx/xychart';
 
-async function ngspice() {
+async function ngspice(setChartData) {
 	const res = await fetch('/api', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 	});
-	console.log(await res.json());
+	setChartData(await res.json());
 }
 
+const accessors = {
+	xAccessor: (d) => d.x,
+	yAccessor: (d) => d.y,
+};
+
 export default function Page() {
-	return <Button onPress={() => ngspice()}>Press me</Button>;
+	const [chartData, setChartData] = useState();
+	console.log(chartData);
+	return (
+		<div>
+			<Button onPress={() => ngspice(setChartData)}>Press me</Button>
+			{chartData && (
+				<XYChart
+					height={300}
+					xScale={{ type: 'band' }}
+					yScale={{ type: 'linear' }}
+				>
+					<Axis orientation='bottom' />
+					<Axis orientation='left' />
+					<Grid columns={false} numTicks={4} />
+					{chartData.map((datum, index) => (
+						<LineSeries
+							key={index}
+							dataKey='Line 1'
+							data={datum}
+							{...accessors}
+						/>
+					))}
+				</XYChart>
+			)}
+		</div>
+	);
 }
