e7756b038c381a7c1b3b7120d8230051e2cedfbe
Jim Jin <jim_king_2000@163.com>
2024-01-23 14:21:19 +0800

    four charts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Button } from '@nextui-org/react';
+import groupBy from 'lodash/groupBy';
 import { useState } from 'react';
+import { Button } from '@nextui-org/react';
 import { Axis, Grid, LineSeries, XYChart, Tooltip } from '@visx/xychart';
 
 async function ngspice(setChartData) {
-	const res = await fetch('/api', {
+	const res = await fetch('/api/idvd', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -19,48 +20,43 @@ const accessors = {
 	yAccessor: (d) => d.y,
 };
 
+function Chart({ data }) {
+	const chartData = groupBy(data, 'g');
+	return (
+		<XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+			<Axis orientation='bottom' label='Vds(V)' />
+			<Axis orientation='left' label='Ids(A)' />
+			<Grid columns={false} numTicks={4} />
+			{Object.entries(chartData).map(([g, datum]) => (
+				<LineSeries key={g} dataKey={`Line ${g}`} data={datum} {...accessors} />
+			))}
+			<Tooltip
+				snapTooltipToDatumX
+				snapTooltipToDatumY
+				showVerticalCrosshair
+				showSeriesGlyphs
+				renderTooltip={({ tooltipData, colorScale }) => (
+					<div>
+						<div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
+							{tooltipData.nearestDatum.key}
+						</div>
+						{accessors.xAccessor(tooltipData.nearestDatum.datum)}
+						{', '}
+						{accessors.yAccessor(tooltipData.nearestDatum.datum)}
+					</div>
+				)}
+			/>
+		</XYChart>
+	);
+}
+
 export default function Page() {
 	const [chartData, setChartData] = useState();
 	return (
 		<div>
 			<Button onPress={() => ngspice(setChartData)}>Press me</Button>
-			{chartData && (
-				<XYChart
-					height={300}
-					xScale={{ type: 'band' }}
-					yScale={{ type: 'linear' }}
-				>
-					<Axis orientation='bottom' label='Vds(V)' />
-					<Axis orientation='left' label='Ids(A)' />
-					<Grid columns={false} numTicks={4} />
-					{chartData.map((datum, index) => (
-						<LineSeries
-							key={index}
-							dataKey={`Line ${index}`}
-							data={datum}
-							{...accessors}
-						/>
-					))}
-					<Tooltip
-						snapTooltipToDatumX
-						snapTooltipToDatumY
-						showVerticalCrosshair
-						showSeriesGlyphs
-						renderTooltip={({ tooltipData, colorScale }) => (
-							<div>
-								<div
-									style={{ color: colorScale(tooltipData.nearestDatum.key) }}
-								>
-									{tooltipData.nearestDatum.key}
-								</div>
-								{accessors.xAccessor(tooltipData.nearestDatum.datum)}
-								{', '}
-								{accessors.yAccessor(tooltipData.nearestDatum.datum)}
-							</div>
-						)}
-					/>
-				</XYChart>
-			)}
+			{chartData &&
+				chartData.map((datum, index) => <Chart key={index} data={datum} />)}
 		</div>
 	);
 }
