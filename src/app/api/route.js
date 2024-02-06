35d578f0400fad0c3167950a5270983a3ce469c8
Jim Jin <jim_king_2000@163.com>
2024-02-06 10:44:16 +0800

    redirect to getAllFiles

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,11 +1,20 @@
+const HOST = `http://${process.env.BACKEND_HOST}:5000/`;
+
 export async function POST(request) {
-	const res = await fetch(`http://${process.env.BACKEND_HOST}:5000/`, {
+	const res = await fetch(HOST, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(await request.json()),
 	});
+
+	const data = await res.json();
+	return Response.json(data);
+}
+
+export async function GET(request) {
+	const res = await fetch(HOST);
 
 	const data = await res.json();
 	return Response.json(data);
